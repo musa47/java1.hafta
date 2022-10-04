31591be43b208859063aa71bfc2ac4eee6acfb05
musa47 <68821398+musa47@users.noreply.github.com>
2022-10-04 21:45:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,64 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] krediler) {
+		System.out.println("Hello world!");
+		String ortaMetin="İlginizi çekebilir";
+		 String altMetin="Vade süresş";
+		 System.out.println(ortaMetin);
+		 
+		 int vade =12;
+		 double dolarDun =18.14;
+		 double dolarBugun=18.10;
+		 
+		 boolean dolarDustuMu= false;
+		 
+		 String okYonu= "okYonu";
+		 
+		 if (dolarBugun<dolarDun) { //true
+			 
+			 String okYonu1="down.svg";
+			 System.out.println(okYonu1);
+			
+		} else if(dolarBugun>dolarDun);
+			String okYonu1="up.svg";
+			 System.out.println(okYonu1);	 
+			
+	
+	
+	
+			 //array
+			 
+			 String[] krediler1= {"Hızlı Kredi","Maaşını Halkbantan Alanlar","Mutlu Emekli"};
+			 
+			 System.out.print(krediler1[0]);
+			 
+			 
+			 for (int i = 0; i < krediler1.length; i++) {
+				 System.out.print(krediler1[i]);
+				
+			}
+
+			
+			 
+			 
+			 
+			 {
+				
+
+			}
+
+			}
+			 
+			 
+			 
+			 
+			 
+			 
+	
+		 
+
+	}
+
+
